52c7f01315c1fdaecebf729abcc9634d55508d1b
Central CRS <oscar2021almeida@gmail.com>
2026-06-26 03:46:50 -0400

    Radar Táctico Definitivo con Auto SVG

---
+++ conductores.js
@@ -0,0 +1,62 @@
+const FLOTA = {
+    "SVDK15": {
+        "nombre": "MARCELO CEPEDA",
+        "huella": "", // <--- PEGUE EL CÓDIGO AQUÍ (Ej: "A7X-99B")
+        "telefono": "+56991616481",
+        "ppu": "SVDK-15",
+        "modelo": "KIA SPORTAGE",
+        "foto": "flota/svdk15/chofer.jpg",
+        "foto_vehiculo": "flota/svdk15/auto.jpg"
+    },
+    "BCXZ13": {
+        "nombre": "OSCAR TARRAGÓ VEGA",
+        "huella": "", // <--- PEGUE EL CÓDIGO AQUÍ
+        "telefono": "+56983538631",
+        "ppu": "BCXZ-13",
+        "modelo": "HYUNDAI SONATA",
+        "foto": "flota/bcxz13/chofer.jpg",
+        "foto_vehiculo": "flota/bcxz13/auto.jpg"
+    },
+    "SVDD42": {
+        "nombre": "SERGIO DURÁN MOLINA",
+        "huella": "", // <--- PEGUE EL CÓDIGO AQUÍ
+        "telefono": "+56971014800",
+        "ppu": "SVDD-42",
+        "modelo": "KIA SPORTAGE",
+        "foto": "flota/svdd42/chofer.jpg",
+        "foto_vehiculo": "flota/svdd42/auto.jpg"
+    },
+    "FYFZ60": {
+        "nombre": "Oscar Rojas",
+        "huella": "2PW-00X", // <--- PEGUE EL CÓDIGO AQUÍ
+        "telefono": "+56983464623",
+        "ppu": "FYFZ-60",
+        "modelo": "HYUNDAI SONATA FULL",
+        "whatsapp": "",
+        "facebook": "",
+        "instagram": "",
+        "foto": "flota/fyfz60/chofer.jpg",
+        "foto_vehiculo": "flota/fyfz60/auto.jpg"
+    },
+    "HTKY38": {
+        "nombre": "Cristian Sepulveda",
+        "huella": "", // <--- PEGUE EL CÓDIGO AQUÍ
+        "telefono": "+56951135853",
+        "ppu": "HTKY-38",
+        "modelo": "Subaru legacy",
+        "whatsapp": "",
+        "facebook": "",
+        "instagram": "",
+        "foto": "flota/htky38/chofer.jpg",
+        "foto_vehiculo": "flota/htky38/auto.jpg"
+    },
+    "VLHT30": {
+        "nombre": "JORGE GONZALEZ F",
+        "huella": "", // <--- PEGUE EL CÓDIGO AQUÍ
+        "telefono": "+56996384077",
+        "ppu": "VLHT-30",
+        "modelo": "SUBARU OUTBACK",
+        "foto": "flota/vlht30/chofer.jpg",
+        "foto_vehiculo": "flota/vlht30/auto.jpg"
+    }
+};
